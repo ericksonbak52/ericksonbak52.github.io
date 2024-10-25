c2ac0f9c769a89681f9d00ab66cf255dc3fd973b
ericksonbak52 <89548995+ericksonbak52@users.noreply.github.com>
2024-10-24 20:17:51 -0400

    i did things

--- Jerry.js
+++ Jerry.js
@@ -1,4 +1,20 @@
 const taskInput = document.getElementById("inputBox");
+const listContainer = document.getElementById("listContainer");
+const li = document.createElement("li");
 function addTask(){
-
+    const task = taskInput.value.trim();
+    if (!task) {
+        alert("your a moron do the thing stupid");
+        return;
+    }
+    li.innerHTML = `
+        <label>
+            <input type="checkbox">
+            <span>${task}</span>
+        </label>
+        <span class="edit-btn">Edit</span>
+        <span class="delete-btn">Delete</span>
+`;
+    listContainer.appendChild(li);
+    inputBox.value = "";
 }
