a9a3cfaa1807878ce0b4421c92f17fb1f6b4daf8
ericksonbak52 <89548995+ericksonbak52@users.noreply.github.com>
2024-10-28 21:42:45 -0400

    I changed stuff

--- Jerry.js
+++ Jerry.js
@@ -1,12 +1,18 @@
 const taskInput = document.getElementById("inputBox");
 const listContainer = document.getElementById("listContainer");
-const li = document.createElement("li");
-function addTask(){
+
+function addTask() {
+    // get input from text box
     const task = taskInput.value.trim();
+    const li = document.createElement("li");
+    
+    //checking if input is empty
     if (!task) {
-        alert("your a moron do the thing stupid");
+        alert("your a moron go do something stupid");
         return;
     }
+    
+    //setting the elements html settings
     li.innerHTML = `
         <label>
             <input type="checkbox">
@@ -15,6 +21,9 @@ function addTask(){
         <span class="edit-btn">Edit</span>
         <span class="delete-btn">Delete</span>
 `;
+    
+    //adding element
     listContainer.appendChild(li);
+    //resetting input value
     inputBox.value = "";
-}
+    }
